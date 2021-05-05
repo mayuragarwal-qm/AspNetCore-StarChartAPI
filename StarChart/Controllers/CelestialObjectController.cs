0065950cb8e8649ea37deab79945a146c14d7a35
Mayur Agarwal <49963107+mayuragarwal-qm@users.noreply.github.com>
2021-05-05 23:25:22 +0530

    readonly

--- StarChart/Controllers/CelestialObjectController.cs
+++ StarChart/Controllers/CelestialObjectController.cs
@@ -11,7 +11,7 @@ namespace StarChart.Controllers
     [ApiController]
     public class CelestialObjectController : ControllerBase
     {
-        private ApplicationDbContext _context;
+        private readonly ApplicationDbContext _context;
 
         public CelestialObjectController(ApplicationDbContext context)
         {
